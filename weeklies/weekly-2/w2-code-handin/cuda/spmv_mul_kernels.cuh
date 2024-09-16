4e002e15d879cf0ff96a4d0887a9495b7df7dd68
zch403 <rasmus.kiel@gmail.com>
2024-09-16 20:32:44 +0200

    Update spmv_mul_kernels.cuh

--- weeklies/weekly-2/w2-code-handin/cuda/spmv_mul_kernels.cuh
+++ weeklies/weekly-2/w2-code-handin/cuda/spmv_mul_kernels.cuh
@@ -3,22 +3,22 @@
 
 __global__ void
 replicate0(int tot_size, char* flags_d) {
-    // TODO: fill in your implementation here ...
+    for(int i=0; i<tot_size; i++) { flags_d[i] = 0; }
 }
 
 __global__ void
 mkFlags(int mat_rows, int* mat_shp_sc_d, char* flags_d) {
-    // TODO: fill in your implementation here ...
+    for(int i=0; i<mat_rows; i++) { flags_d[mat_shp_sc_d[i]] = 1; }
 }
 
 __global__ void
 mult_pairs(int* mat_inds, float* mat_vals, float* vct, int tot_size, float* tmp_pairs) {
-    // TODO: fill in your implementation here ...
+    for(int i=0; i<tot_size; i++) { tmp_pairs[i] = mat_vals[i]*vct[mat_inds[i]] }
 }
 
 __global__ void
 select_last_in_sgm(int mat_rows, int* mat_shp_sc_d, float* tmp_scan, float* res_vct_d) {
-    // TODO: fill in your implementation here ...
+    for(int i=0; i<mat_rows; i++) { res_vct_d[i] = tmp_scan[mat_shp_sc_d[i]]; }
 }
 
 #endif // SPMV_MUL_KERNELS
