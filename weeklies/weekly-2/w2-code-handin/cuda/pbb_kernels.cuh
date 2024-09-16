3365600318f0bd5ee8f7173435a13bdbf3694c9f
zch403 <rasmus.kiel@gmail.com>
2024-09-16 22:04:39 +0200

    Update pbb_kernels.cuh

--- weeklies/weekly-2/w2-code-handin/cuda/pbb_kernels.cuh
+++ weeklies/weekly-2/w2-code-handin/cuda/pbb_kernels.cuh
@@ -448,8 +448,8 @@ copyFromGlb2ShrMem( const uint32_t glb_offs
 ) {
     #pragma unroll
     for(uint32_t i=0; i<CHUNK; i++) {
-        //uint32_t loc_ind = blockDim.x*i + threadIdx.x;
-        uint32_t loc_ind = threadIdx.x*CHUNK + i;
+        uint32_t loc_ind = blockDim.x*i + threadIdx.x;
+        //uint32_t loc_ind = threadIdx.x*CHUNK + i;
         uint32_t glb_ind = glb_offs + loc_ind;
         T elm = ne;
         if(glb_ind < N) { elm = d_inp[glb_ind]; }
@@ -479,8 +479,8 @@ copyFromShr2GlbMem( const uint32_t glb_offs
 ) {
     #pragma unroll
     for (uint32_t i = 0; i < CHUNK; i++) {
-        //uint32_t loc_ind = blockDim.x*i + threadIdx.x;
-        uint32_t loc_ind = threadIdx.x*CHUNK + i;
+        uint32_t loc_ind = blockDim.x*i + threadIdx.x;
+        //uint32_t loc_ind = threadIdx.x*CHUNK + i;
         uint32_t glb_ind = glb_offs + loc_ind;
         if (glb_ind < N) {
             T elm = const_cast<const T&>(shmem_red[loc_ind]);
