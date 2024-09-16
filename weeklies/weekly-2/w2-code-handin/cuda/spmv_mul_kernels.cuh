c960f508c169e89e2a1b19c81dba27ee7dbbfbfb
zch403 <rasmus.kiel@gmail.com>
2024-09-16 20:44:08 +0200

    Update spmv_mul_kernels.cuh

--- weeklies/weekly-2/w2-code-handin/cuda/spmv_mul_kernels.cuh
+++ weeklies/weekly-2/w2-code-handin/cuda/spmv_mul_kernels.cuh
@@ -3,22 +3,34 @@
 
 __global__ void
 replicate0(int tot_size, char* flags_d) {
-    for(int i=0; i<tot_size; i++) { flags_d[i] = 0; }
+    const unsigned int gid = threadIdx.x+256*blockIdx.x;
+    if (gid < tot_size) {
+        flags_d[gid] = 0;
+    }
 }
 
 __global__ void
 mkFlags(int mat_rows, int* mat_shp_sc_d, char* flags_d) {
-    for(int i=0; i<mat_rows; i++) { flags_d[mat_shp_sc_d[i]] = 1; }
+    const unsigned int gid = threadIdx.x+256*blockIdx.x;
+    if (gid < mat_rows) {
+        flags_d[mat_shp_sc_d[gid]] = 1;
+    }
 }
 
 __global__ void
 mult_pairs(int* mat_inds, float* mat_vals, float* vct, int tot_size, float* tmp_pairs) {
-    for(int i=0; i<tot_size; i++) { tmp_pairs[i] = mat_vals[i]*vct[mat_inds[i]]; }
+    const unsigned int gid = threadIdx.x+256*blockIdx.x;
+    if (gid < tot_size) {
+        tmp_pairs[gid] = mat_vals[gid]*vct[mat_inds[gid]];
+    }
 }
 
 __global__ void
 select_last_in_sgm(int mat_rows, int* mat_shp_sc_d, float* tmp_scan, float* res_vct_d) {
-    for(int i=0; i<mat_rows; i++) { res_vct_d[i] = tmp_scan[mat_shp_sc_d[i]]; }
+    const unsigned int gid = threadIdx.x+256*blockIdx.x;
+    if (gid < mat_rows) {
+        res_vct_d[gid] = tmp_scan[mat_shp_sc_d[gid]];
+    }
 }
 
 #endif // SPMV_MUL_KERNELS
