46bb1aa795f18ba13599c2abc1e8ccacb8ea8490
zch403 <rasmus.kiel@gmail.com>
2024-09-16 22:52:55 +0200

    Update pbb_kernels.cuh

--- weeklies/weekly-2/w2-code-handin/cuda/pbb_kernels.cuh
+++ weeklies/weekly-2/w2-code-handin/cuda/pbb_kernels.cuh
@@ -179,25 +179,25 @@ class Mssp {
 template<class OP>
 __device__ inline typename OP::RedElTp
 scanIncWarp( volatile typename OP::RedElTp* ptr, const unsigned int idx ) {
-    // const unsigned int lane = idx & (WARP-1);
-        
-    // if(lane==0) {
-    //     #pragma unroll
-    //     for(int i=1; i<WARP; i++) {
-    //         ptr[idx+i] = OP::apply(ptr[idx+i-1], ptr[idx+i]);
-    //     }
-    // }
-    // return OP::remVolatile(ptr[idx]);
     const unsigned int lane = idx & (WARP-1);
         
-    #pragma unroll
-    for(int d=0; d<lgWARP; d++) {
-        int h = 1 << d;
-        if (lane >= h) {
-            ptr[idx] = OP::apply(ptr[idx-h], ptr[idx]);
+    if(lane==0) {
+        #pragma unroll
+        for(int i=1; i<WARP; i++) {
+            ptr[idx+i] = OP::apply(ptr[idx+i-1], ptr[idx+i]);
         }
     }
     return OP::remVolatile(ptr[idx]);
+    // const unsigned int lane = idx & (WARP-1);
+        
+    // #pragma unroll
+    // for(int d=0; d<lgWARP; d++) {
+    //     int h = 1 << d;
+    //     if (lane >= h) {
+    //         ptr[idx] = OP::apply(ptr[idx-h], ptr[idx]);
+    //     }
+    // }
+    // return OP::remVolatile(ptr[idx]);
 }
 
 /**
@@ -448,8 +448,8 @@ copyFromGlb2ShrMem( const uint32_t glb_offs
 ) {
     #pragma unroll
     for(uint32_t i=0; i<CHUNK; i++) {
-        // uint32_t loc_ind = threadIdx.x*CHUNK + i;
-        uint32_t loc_ind = blockDim.x*i + threadIdx.x;
+        uint32_t loc_ind = threadIdx.x*CHUNK + i;
+        //uint32_t loc_ind = blockDim.x*i + threadIdx.x;
         uint32_t glb_ind = glb_offs + loc_ind;
         T elm = ne;
         if(glb_ind < N) { elm = d_inp[glb_ind]; }
@@ -480,8 +480,8 @@ copyFromShr2GlbMem( const uint32_t glb_offs
     #pragma unroll
     for (uint32_t i = 0; i < CHUNK; i++) {
         
-        // uint32_t loc_ind = threadIdx.x*CHUNK + i;
-        uint32_t loc_ind = blockDim.x*i + threadIdx.x;
+        uint32_t loc_ind = threadIdx.x*CHUNK + i;
+        //uint32_t loc_ind = blockDim.x*i + threadIdx.x;
         uint32_t glb_ind = glb_offs + loc_ind;
         if (glb_ind < N) {
             T elm = const_cast<const T&>(shmem_red[loc_ind]);
