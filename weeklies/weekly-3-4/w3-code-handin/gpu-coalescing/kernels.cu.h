ee7b18d6c47f7d2045e880f0ace1e92c88501f15
zch403 <rasmus.kiel@gmail.com>
2024-10-01 19:52:20 +0200

    Update kernels.cu.h

--- weeklies/weekly-3-4/w3-code-handin/gpu-coalescing/kernels.cu.h
+++ weeklies/weekly-3-4/w3-code-handin/gpu-coalescing/kernels.cu.h
@@ -100,9 +100,9 @@ transKernel(ElTp* A_tr, ElTp* B_tr, uint32_t num_rows, uint32_t num_cols) {
 
     ElTp accum = 0;
     for(int j=0; j<num_cols; j++) {
-        ElTp el_a  = A_tr[ gid*num_cols + j ];
+        ElTp el_a  = A[j*num_rows+ gid ];
         accum = sqrt(accum) + el_a * el_a;
-        B_tr[ gid*num_cols + j ] = accum;
+        B[j*num_rows+ gid ] = accum;
     }
 }
 
